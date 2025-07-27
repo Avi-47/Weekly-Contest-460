946bc34c21d6a3276023e32849dd88b0e1becd7b
Avimanyu Goswami <166274452+Avi-47@users.noreply.github.com>
2025-07-27 13:21:09 +0530

    Update ques_2.java

--- ques_2.java
+++ ques_2.java
@@ -1,42 +1,39 @@
 class Solution {
     public long numOfSubsequences(String s) {
-        long countL = 0, countLC = 0, countLCT = 0;
-        long totalT = 0;
-
-        for (char ch : s.toCharArray()) {
-            if (ch == 'T') totalT++;
+        int n = s.length();
+        long countL = 0;
+        long countLC = 0;
+        long countLCT = 0;
+        for(char c:s.toCharArray()){
+            if(c=='L') countL++;
+            else if(c=='C') countLC+=countL;
+            else if(c=='T') countLCT+=countLC;
         }
-
-        long bestInsertC = 0;
-        long bestInsertT = 0;
-        long remainingT = totalT;
-
-        for (char ch : s.toCharArray()) {
-            if (ch == 'L') {
-                countL++;
-            } else if (ch == 'C') {
-                countLC += countL;
-            } else if (ch == 'T') {
-                countLCT += countLC;
-                remainingT--; 
+        long[] suffixT = new long[n+1];
+        long[] suffixCT = new long[n+1];
+        for(int i=n-1;i>=0;i--){
+            char ch = s.charAt(i);
+            suffixT[i]=suffixT[i+1];
+            suffixCT[i]=suffixCT[i+1];
+            if(ch=='T') suffixT[i]++;
+            if(ch=='C') suffixCT[i]+=suffixT[i];
+        }
+        long extra = 0;
+        long prefixL=0;
+        long prefixLC=0;
+        for(int i=0;i<=n;i++){
+            // insert L
+            extra = Math.max(extra,suffixCT[i]);
+            // insert C
+            extra = Math.max(extra,prefixL*suffixT[i]);
+            //insert T
+            extra = Math.max(extra,prefixLC);
+            if(i<n){
+                char ch = s.charAt(i);
+                if(ch=='L') prefixL++;
+                if(ch=='C') prefixLC+=prefixL;
             }
-
-            bestInsertC = Math.max(bestInsertC, countL * remainingT);
-
-            bestInsertT = Math.max(bestInsertT, countLC);
         }
-
-        long countC = 0, boostByL = 0;
-        for (char ch : s.toCharArray()) {
-            if (ch == 'C') countC++;
-            else if (ch == 'T') boostByL += countC;
-        }
-
-        long result = countLCT;
-        result = Math.max(result, countLCT + boostByL);      
-        result = Math.max(result, countLCT + bestInsertC);   
-        result = Math.max(result, countLCT + bestInsertT);   
-
-        return result;
+        return countLCT+extra;
     }
 }
