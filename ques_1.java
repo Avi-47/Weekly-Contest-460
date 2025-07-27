e4d7de323e9e2fb8d1eb92210642fac63d5898dc
Avimanyu Goswami <166274452+Avi-47@users.noreply.github.com>
2025-07-27 12:24:38 +0530

    Create ques_1.java

---
+++ ques_1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long maximumMedianSum(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        long sum = 0;
+        for (int i=n-2; i>=n/3; i-=2) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+}
